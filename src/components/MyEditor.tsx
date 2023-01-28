95cc59a15c1295e92a0fc424e4f8e69a68fa5800
vlsk3104 <vlsk3104@gmail.com>
2023-01-28 22:43:35 +0900

    保存した内容の取り出す

--- src/components/MyEditor.tsx
+++ src/components/MyEditor.tsx
@@ -1,11 +1,21 @@
-import { useState } from 'react';
-import { Editor, EditorState, convertToRaw } from 'draft-js';
+import { useEffect, useState } from 'react';
+import { Editor, EditorState, convertFromRaw, convertToRaw } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
 function MyEditor() {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
+
+  useEffect(() => {
+    const raw = localStorage.getItem('test');
+    if (raw) {
+      const contentState = convertFromRaw(JSON.parse(raw));
+      const newEditorState = EditorState.createWithContent(contentState);
+      setEditorState(newEditorState);
+    }
+  }, []);
+
   const saveContent = () => {
     const contentState = editorState.getCurrentContent();
     const raw = convertToRaw(contentState);
