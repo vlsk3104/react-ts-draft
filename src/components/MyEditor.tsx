c3c88a1b03c8f6479881c9f10581c5b7ff959770
vlsk3104 <vlsk3104@gmail.com>
2023-01-28 22:28:30 +0900

    入力した内容を取得する

--- src/components/MyEditor.tsx
+++ src/components/MyEditor.tsx
@@ -1,18 +1,28 @@
 import { useState } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, convertToRaw } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
 function MyEditor() {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
+  const saveContent = () => {
+    const contentState = editorState.getCurrentContent();
+    const raw = convertToRaw(contentState);
+    console.log(raw);
+  };
 
   return (
-    <Editor
+    <div>
+      <div>
+        <button onClick={saveContent}>保存</button>
+      </div>
+      <Editor
       editorState={editorState}
       onChange={setEditorState}
       placeholder="ここから入力を行ってください。"
     />
+    </div>
   );
 }
 export default MyEditor;
